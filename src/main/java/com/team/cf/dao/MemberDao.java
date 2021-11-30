2fdf5c465e75195d32ff4466edccedd8b0b36e50
cjyniubi <1691684781@qq.com>
2021-11-30 19:48:50 +0800

    cjy第一次提交

--- src/main/java/com/team/cf/dao/MemberDao.java
+++ src/main/java/com/team/cf/dao/MemberDao.java
@@ -1,6 +1,27 @@
 package com.team.cf.dao;
 
 
+import com.team.cf.entity.Member;
+
+import java.util.List;
 
 public interface MemberDao {
+
+    //查询所有用户
+    public List<Member> selectAll(Object... params) throws Exception;
+
+    //查单个
+    public Member selectOne(Object... params) throws Exception;
+
+    //增
+    public int insertMember(Member member) throws Exception;
+
+    //删
+    public int deleteMember(Member member) throws Exception;
+
+    //改
+    public int updateMember(Member member) throws Exception;
+
+    //改实名认证状态
+    public int updateMemberAuthStatus(Member member) throws Exception;
 }
--- src/main/java/com/team/cf/dao/impl/MemberDaoImpl.java
+++ src/main/java/com/team/cf/dao/impl/MemberDaoImpl.java
@@ -1,10 +1,57 @@
 package com.team.cf.dao.impl;
 
+import com.team.cf.dao.BaseDao;
+import com.team.cf.dao.MemberDao;
+import com.team.cf.entity.Member;
+
+import java.util.List;
+
 /**
  * @Description TODO
- * @Author shejiatao
- * @Date 2021/11/29 19:00
+ * @Author chenJunYu
+ * @Date 2021/11/29 20:08
  */
-public class MemberDaoImpl {
+public class MemberDaoImpl extends BaseDao<Member> implements MemberDao {
+    @Override
+    public List<Member> selectAll(Object... params) throws Exception {
+        return null;
+    }
 
+    @Override
+    public Member selectOne(Object... params) throws Exception {
+        System.out.println("MemberDaoImpl 查询");
+        String sql = "select * from t_member where loginacct = ?";
+        Member member = this.getBean(sql, Member.class, params);
+        System.out.println("member = "+ member);
+        return member;
+    }
+
+    //插入一个人
+    @Override
+    public int insertMember(Member member) throws Exception {
+        System.out.println("MemberDaoImpl 插入");
+        String sql = "insert into t_member values(default,?,?,?,?,0,?,?,?,?)";
+        int i = this.update(sql,
+                member.getLoginacct(), member.getUserpswd(), member.getUsername(), member.getEmail(),
+                member.getUsertype(), member.getRealname(), member.getCardnum(), member.getAccttype());
+        System.out.println("MemberDaoImpl 中 insertMember 的 i = "+i);
+        return i;
+    }
+
+    @Override
+    public int deleteMember(Member member) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int updateMember(Member member) throws Exception {
+        return 0;
+    }
+
+    //修改实名认证的状态 实名认证状态0一未实名认证，1 -实名认证申请中，| 2一已实名认证
+    @Override
+    public int updateMemberAuthStatus(Member member) throws Exception {
+        String sql = "update t_member set authstatus = ？ where ";
+        return 0;
+    }
 }
